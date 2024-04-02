5755ac47df0c6f86cda81e119397a451f73aad63
Prajwal Kumar <kumpraj@gmail.com>
2024-04-02 06:22:21 +0530

    added error handler middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,28 +1,29 @@
-import express, {Request, Response}  from 'express';
-import {body, validationResult} from 'express-validator';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-    body('email')
-        .isEmail()
-        .withMessage('Email must be valid'),
-    body('password')
-        .trim()
-        .isLength({min: 4, max: 20})
-        .withMessage('Password must be between 4 and 20 characters')
-], (req:Request ,res:Response) => {
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
     const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-        return res.status(400).send(errors.array());
+    if (!errors.isEmpty()) {
+      throw new Error("Invalid email or password");
     }
 
-    const {email, password} = req.body;
-
-    console.log('Creating user...');
+    console.log("Creating a user...");
+    throw new Error("Error connecting to database");
 
     res.send({});
-});
+  }
+);
 
 export { router as signupRouter };
