39e83587c648cc3b325f0fc1923b6f99ae76f4db
Prajwal Kumar <kumpraj@gmail.com>
2024-04-06 00:49:09 +0530

    generating JWT

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { json } from "body-parser";
 import 'express-async-errors';
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -11,7 +12,17 @@ import { NotFoundError } from "./errors/not-found-error";
 import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
+app.set('trust proxy', true); // trust nginx proxy 
 app.use(json());
+
+// secure cookie requires https connection, else the cookie will not be set
+// signed: false - since we don't really care about the cookie value, just its existence
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
